f0ec30b2f9b4d11e86d01413696377c0cdb81c35
LeonardoEmmanuel <133007838+LeonardoEmmanuel@users.noreply.github.com>
2025-02-28 11:19:00 -0300

    sd

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
  
-    botaoDeAcessibilidade.addEventListener('click', function () {
+    // Evento para abrir/fechar o menu de acessibilidade
+    botaoDeAcessibilidade.addEventListener('click', function() {
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+
+        // Alterna o estado do atributo aria-expanded
         const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
         botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
     });
  
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-     
     const alternaContraste = document.getElementById('alterna-contraste');
  
     let tamanhoAtualFonte = 1;
  
-    aumentaFonteBotao.addEventListener('click', function () {
+    // Evento para aumentar o tamanho da fonte
+    aumentaFonteBotao.addEventListener('click', function() {
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
  
-    diminuiFonteBotao.addEventListener('click', function () {
+    // Evento para diminuir o tamanho da fonte
+    diminuiFonteBotao.addEventListener('click', function() {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
  
-    alternaContraste.addEventListener('click', function () {
+    // Evento para alternar o contraste do site
+    alternaContraste.addEventListener('click', function() {
         document.body.classList.toggle('alto-contraste');
     });
-
-    ScrollReveal().reveal('#inicio', { delay: 500 });
-    ScrollReveal().reveal('#tropicalia', { delay: 500 });
-    ScrollReveal().reveal('#galeria', { delay: 500 });
-    ScrollReveal().reveal('#contato', { delay: 500 });
 });
+ 
+// Revela as seções ao rolar a página
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
